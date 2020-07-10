515f54b4c730a24b80ecf7ae5f02284f41822dd6
Coldiblaster <vbastazin@gmail.com>
2020-07-10 14:17:38 -0300

    chore: add dir coverage

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**'
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
