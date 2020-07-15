5929f3408f6424e9b521e3452bad5941adadeb5f
Coldiblaster <vbastazin@gmail.com>
2020-07-15 15:15:26 -0300

    chore: add jest configs and scripts

---
+++ jest-unit-config.js
@@ -0,0 +1,9 @@
+const config = require('./jest.config')
+config.testMatch = ['**/*.spec.ts']
+module.exports = config
+
+/**
+ *  Estamos pegando tudo que tem no jest.config e adicionando uma configuração nova.
+ *  Configuramos o Jest para rodar qualquer arquivo com o spec.ts na configuração acima
+ *  Isso foi feito para configurarmos o script de inicialização para rodar somente os teste unitarios
+ */
